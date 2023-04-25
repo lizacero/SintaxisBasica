b4807f7b385166cf8e3fc2720e03b2ac70b84291
Daniela Acero <danielalizethacero@gmail.com>
2023-04-24 21:34:19 -0500

    nueva carpeta de actividades

---
+++ actividades/script.js
@@ -0,0 +1,24 @@
+/* Actividad 1
+Defina dos variables: Nombre y edad
+Imprimalas por consola */
+console.log("Actividad 1")
+var nombre = "Daniela"
+var edad = 25
+
+console.log(nombre)
+console.log(edad)
+
+/* Actividad 2
+1. Defina dos variables globales: nombre y apellido.
+2. Defina dos variables locales en un bloque: edad y comida.
+3. Pruebe cambiando las variables por fuera y por dentro del bloque e imprimiendolas por consola.
+4. Una vez entienda la diferencia entre let y var, responda la siguiente pregunta: 
+    ¿En qué casos resulta mejor usar let o var para definir variables? */
+console.log("Actividad 2")
+var nombre = "Daniela"
+var apellido = "Acero"
+{
+    let edad = 25
+    let comida = "Pasta"
+}
+
